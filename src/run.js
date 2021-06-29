f3c7f88090ff9efc7f3c8e916bc24e63b344cd74
Richard Case <richard@weave.works>
2021-06-29 12:50:18 +0100

    feat: started to add regex checking
    Signed-off-by: Richard Case <richard@weave.works>

--- src/run.js
+++ src/run.js
@@ -3,16 +3,28 @@ const core = require('@actions/core');
 
 module.exports = async function run() {
     try {
-        //const token  = core.getInput('repo-token');
-        //const client = new github.GitHub(process.env.GITHUB_TOKEN)
-        const contextPullRequest = github.context.payload.pull_request;
-        if (!contextPullRequest) {
+        const ctx = github.context;
+        
+        if (ctx.eventName != "pull_request") {
             throw new Error(
-                "This action can only be invoked in `pull_request` events. Otherwise the pull request can't be inferred."
+                "This action can only be invoked in `pull_request` events."
             );
         }
-        console.log(`The event payload: ${JSON.stringify(contextPullRequest, undefined, 2)}`);
-        console.log(`The context: ${JSON.stringify(github.context, undefined, 2)}`);
+
+        const payload = github.context.payload;
+        htmlURL = payload.html_url;
+        body = payload.body;
+
+        if (body === "") {
+            throw new Error("You must supply a description to the PR that includes a `release-note` code block");
+        }
+
+        const found = body.search(/(?:Release note*\*:s*(?:<!--[^<>]*-->s*)?\`\`\`(?:release-note)?|\`\`\`release-note)(.+?)\`\`\`/g);
+
+        console.log(found);
+
+        //console.log(`The event payload: ${JSON.stringify(payload, undefined, 2)}`);
+        //console.log(`The context: ${JSON.stringify(github.context, undefined, 2)}`);
 
     } catch (error) {
         core.setFailed(error.message);
--- src/run.test.js
+++ src/run.test.js
@@ -1,48 +1,54 @@
-const run = require('./run');
-
-
-const core = require('@actions/core')
-const request = require('request');
+//const core = require('@actions/core')
+//const request = require('request');
 
 const mocha = require('mocha');
 
-const sinon = require('sinon');
+//const sinon = require('sinon');
 const { expect } = require('chai');
-const sandbox = sinon.createSandbox();
+//const sandbox = sinon.createSandbox();
 const fs = require('fs');
+const path = require('path');
 
-let getInput = null;
+//let getInput = null;
 
 describe('Run', () => {
     afterEach(() => {
         console.log('after each');
-        sandbox.restore();
+        //sandbox.restore();
     })
 
     const setup = () => {
         console.log('in setup');
-        getInput = sandbox.stub(core, 'getInput');
-        getInput.withArgs('github-token').returns('v1.asdf');
+        //getInput = sandbox.stub(core, 'getInput');
+        //getInput.withArgs('github-token').returns('v1.asdf');
 
         process.env.GITHUB_RUN_ID = "1234567"
-        process.env.GITHUB_SHA = "asdfasdf"
-        process.env.GITHUB_REF = "master"
+        process.env.GITHUB_SHA = "0df36e327f56c8897894b9f52734c69b56ff6e79"
+        process.env.GITHUB_REF = "refs/pull/5/merge"
         process.env.GITHUB_EVENT_NAME = "pull_request"
+        process.env.GITHUB_WORKFLOW = "pr_size"
+        process.env.GITHUB_ACTION = "richardcaserelease-note-action"
+        process.env.GITHUB_ACTOR = "actionsbot"
+        process.env.GITHUB_JOB = "labeler"
+        process.env.GITHUB_RUN_NUMBER = "5"
+        process.env.GITHUB_RUN_ID = "982249112"
+        process.env.GITHUB_REPOSITORY = "test/repo"
 
-        const eventPath = "../test/sample.json"
+
+        const eventPath =  path.join(__dirname, "../test/sample.json");
         process.env.GITHUB_EVENT_PATH = eventPath
 
         // set PR number:
-        const readFileSync = sandbox.stub(fs,'readFileSync')
-        readFileSync.withArgs(eventPath, 'utf8').returns(
-            JSON.stringify({ number: 1 })
-        )
+        //const readFileSync = sandbox.stub(fs,'readFileSync')
+        //readFileSync.withArgs(eventPath, 'utf8').returns(
+        //    JSON.stringify({ number: 1 })
+        //)
     }
 
     it('should get the pr', function() {
         setup();
 
-        sandbox.stub(request, 'post');
+        const run = require('./run');
 
         run().then(
             (result) => {
