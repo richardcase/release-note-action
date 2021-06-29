b5979e5eb4ab1962ff2442d59d42d8a859bb01ca
Richard Case <richard@weave.works>
2021-06-29 15:20:24 +0100

    refactor: changed from node to Go
    Signed-off-by: Richard Case <richard@weave.works>

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    clearMocks: true,
-    moduleFileExtensions: ['js'],
-    testEnvironment: 'node',
-    testMatch: ['**/*.test.js'],
-    testRunner: 'jest-circus/runner',
-    verbose: true
-  }
